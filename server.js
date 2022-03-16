abe1b7a8f5627227fe88ad52d3ca79b937ee92bd
pdhende <pdhendework@gmail.com>
2022-03-15 20:48:24 -0400

    Updated server.js to include get routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
@@ -7,15 +8,24 @@ const PORT = 3001;
 // To serve static files from the public folder.
 app.use(express.static('public'));
 
+// Default route 
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // Route to serve the notes.html page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//WIldcard route to serve the index.html page
+//Wildcard route to serve the index.html page
 app.get('/*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+// Route to get notes from the db.json file
+app.get('/api/notes', (req, res) => res.json(notes));
+
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
